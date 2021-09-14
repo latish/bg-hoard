c729168ae1c379e394bde9c8061e6b44ba806726
Latish Sehgal <latish.sehgal@gmail.com>
2021-09-14 10:43:59 -0500

    lab 2

--- bg-hoard/apps/store/src/app/app.module.ts
+++ bg-hoard/apps/store/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-  ],
+  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }), MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
