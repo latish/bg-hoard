8e41ff22c300f477da2cecf270544927afe4c51e
Latish Sehgal <latish.sehgal@gmail.com>
2021-09-15 10:51:07 -0500

    implement generator

--- bg-hoard/tools/generators/util-lib/index.ts
+++ bg-hoard/tools/generators/util-lib/index.ts
@@ -1,8 +1,19 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
+interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
+}
+
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  console.log('=========', `util-${schema.name}`);
+
+  await libraryGenerator(tree, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`,
+  });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
